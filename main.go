f467d1a95efa3ad25a055466f264d88c96d86865
Yarpopcat08 <109970976+Yarpopcat08@users.noreply.github.com>
2026-07-10 17:18:53 +0300

    Add dbserivce, dotenv, start work

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/VsAltAuth/VSAA/services"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	fmt.Println("Hi")
+	services.DbInit()
+	router := gin.Default()
+	//router.TrustedPlatform = gin.PlatformCloudflare
+	router.GET("/", func(c *gin.Context) { c.JSON(http.StatusOK, gin.H{"message": "hi"}) })
+	router.GET("/resolveplayername")
+
+	router.Run("localhost:8080")
+}
---
+++ services/dbservice.go
@@ -0,0 +1,49 @@
+package services
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+
+	_ "github.com/joho/godotenv/autoload"
+)
+
+var DATABASE string = os.Getenv("SQLITE_PATH")
+
+type User struct {
+	gorm.Model
+	UID          string
+	Email        string
+	HashedPass   string
+	Username     string
+	Entitlements string
+}
+
+type Session struct {
+	gorm.Model
+	UID        string
+	Sessionkey string
+	Gamever    string
+}
+
+var ctx = context.Background()
+
+func DbInit() {
+	fmt.Println("hi from dbinit")
+	db, err := gorm.Open(sqlite.Open(DATABASE), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	db.AutoMigrate(&User{}, &Session{})
+	//mkusr(db)
+}
+
+func mkusr(db *gorm.DB) {
+	err := gorm.G[User](db).Create(ctx, &User{UID: "niggerniggernigger", Email: "megadfga111@xyecoc.com", HashedPass: "dneh", Username: "yar", Entitlements: "VIV"})
+	if err != nil {
+		print(err)
+	}
+}
