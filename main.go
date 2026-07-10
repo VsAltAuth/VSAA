fbf855eb36fdd967e639088d5fad33524c91f124
Yarpopcat08 <109970976+Yarpopcat08@users.noreply.github.com>
2026-07-10 21:48:22 +0300

    readd DbInit and declare CacheService

--- main.go
+++ main.go
@@ -5,15 +5,18 @@ import (
 	"net/http"
 
 	//"github.com/VsAltAuth/VSAA/services"
+	"github.com/VsAltAuth/VSAA/services"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	fmt.Println("Hi")
+	db := services.DbInit()
+	CacheService := services.NewCacheService(db, 5, 10)
 	router := gin.Default()
 	//router.TrustedPlatform = gin.PlatformCloudflare
 	router.GET("/", func(c *gin.Context) { c.JSON(http.StatusOK, gin.H{"message": "hi"}) })
-	router.GET("/resolveplayername")
+	router.GET("/resolveplayername", services.)
 
 	router.Run("localhost:8080")
 }
