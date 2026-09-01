46a17b0542c93a47b887e6f5514421c511beb1be
Yarpopcat08 <109970976+Yarpopcat08@users.noreply.github.com>
2026-09-01 15:23:43 +0300

    feat[cryptUtils]: refactor

--- utils/cryptUtils.go
+++ utils/cryptUtils.go
@@ -8,11 +8,17 @@ import (
 	"encoding/pem"
 	"fmt"
 	"os"
+	"sync"
 
 	_ "github.com/joho/godotenv/autoload"
 
 	"golang.org/x/crypto/bcrypt"
 )
+
+var publicKey string
+var privateKey *rsa.PrivateKey
+var pubKeyOnce sync.Once
+var privKeyOnce sync.Once
 
 func privkeyFile() string {
 	file := os.Getenv("PRIVKEY_FILE")
@@ -22,41 +28,60 @@ func privkeyFile() string {
 	return file
 }
 
-func PubkeyFile() string {
-	file := os.Getenv("PUBKEY_FILE")
-	if file == "" {
-		return "public_key.pem"
-	}
-	return file
+func GetPublicKey() string {
+	pubKeyOnce.Do(func() {
+		if privateKey == nil {
+			GetPrivateKey()
+		}
+		pubKey := &privateKey.PublicKey
+		pubKeyBytes, err := x509.MarshalPKIXPublicKey(pubKey)
+		if err != nil {
+			fmt.Printf("error marshaling public key: %v", err)
+			return
+		}
+		pemBlock := &pem.Block{
+			Type:  "PUBLIC KEY",
+			Bytes: pubKeyBytes,
+		}
+		pemBytes := pem.EncodeToMemory(pemBlock)
+		publicKey = string(pemBytes)
+	})
+	return publicKey
 }
 
-// I'm not sure if throwing panic so much is a good idea... will consult smart people later
-func PrivateKey() (*rsa.PrivateKey, error) {
-	keyData, err := os.ReadFile(privkeyFile())
-	if err != nil {
-		return nil, err
+func GetPrivateKey() (*rsa.PrivateKey, error) {
+	var critError error
+	privKeyOnce.Do(func() {
+		keyData, err := os.ReadFile(privkeyFile())
+		if err != nil {
+			critError = err
+			return
+		}
+		block, _ := pem.Decode(keyData)
+		if block == nil {
+			critError = fmt.Errorf("GetPrivateKey() - decoded PEM data is nil")
+			return
+		}
+		privateKey, err = x509.ParsePKCS1PrivateKey(block.Bytes)
+		if err == nil {
+			critError = err
+			return
+		}
+		critError = err
+	})
+	if critError != nil {
+		return nil, critError
 	}
-	block, _ := pem.Decode(keyData)
-	if block == nil {
-		return nil, fmt.Errorf("privateKey() - decoded PEM data is nil")
-	}
-	privateKey, err := x509.ParsePKCS1PrivateKey(block.Bytes)
-	if err == nil {
-		return privateKey, nil
-	}
-	return nil, err
+	return privateKey, nil
+
 }
 
 func Sign(unsignedval string) (string, error) {
 	val := []byte(unsignedval)
 	hashed := sha256.Sum256(val)
-	privateKey, err := PrivateKey()
-	if err != nil {
-		return "no privatekey", err
-	}
 	signed, err := rsa.SignPKCS1v15(nil, privateKey, crypto.SHA256, hashed[:])
 	if err != nil {
-		return "invalid", err
+		return "", err
 	}
 	signedString := string(signed)
 	return signedString, nil
